0eb7622bc114468c2bc673104e4b5ed99f3d2c1c
Stefan Klarmann <klarmann1@googlemail.com>
2020-12-09 18:00:50 +0100

    add-button functionality added + corresponding button-id

--- script.js
+++ script.js
@@ -16,15 +16,15 @@ const createElement = (name) => {
   //add done event listener
   textSpan.addEventListener("click", () => {
     element.classList.toggle("checked");
-  }); 
-  // addEvent-Listener to supress the return-Key from beeing pressed 
-  textSpan.addEventListener("keydown", keyInfo => {   
-         // stop editing if the Enter-Key is pressed     
-              if(keyInfo.keyCode == 13) {
-                // console.log('the Return-Key on the Keyboard has the keydown-Number: 13!');
-                textSpan.contentEditable = 'false'; 
-              } 
-    });  
+  });
+  // addEvent-Listener to supress the return-Key from beeing pressed
+  textSpan.addEventListener("keydown", (keyInfo) => {
+    // stop editing if the Enter-Key is pressed
+    if (keyInfo.keyCode == 13) {
+      // console.log('the Return-Key on the Keyboard has the keydown-Number: 13!');
+      textSpan.contentEditable = "false";
+    }
+  });
   //add text span to element
   element.appendChild(textSpan);
 
@@ -33,12 +33,12 @@ const createElement = (name) => {
   editSpan.className = "edit"; //TODO: add classes
   editSpan.appendChild(document.createTextNode("edit")); //TODO: replace with icon
   // add event listener to the button
-    editSpan.addEventListener("click", () => {
-        // make the content editable for the textSpan (previousSibling)! 
-        textSpan.contentEditable = 'true';  
-    });  
+  editSpan.addEventListener("click", () => {
+    // make the content editable for the textSpan (previousSibling)!
+    textSpan.contentEditable = "true";
+  });
 
-  //add edit button to element 
+  //add edit button to element
   element.appendChild(editSpan);
 
   //create delete button node
@@ -52,6 +52,20 @@ const createElement = (name) => {
   // add the li node to the todo list
   toDoList.appendChild(element);
 };
+
+// Add Button & Function
+let addButton = document.querySelector("#add");
+
+addButton.addEventListener("click", () => {
+  let name = document.querySelector("#task").value;
+  event.preventDefault();
+  if (name) {
+    createElement(name);
+    document.querySelector("#task").value = "";
+  } else {
+    alert("Please enter a task");
+  }
+});
 
 createElement("test1");
 createElement("test2");
