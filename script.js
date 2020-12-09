450d47ebf007408fb4f7afa9a35051feca556a4b
g-weiher <74683223+g-weiher@users.noreply.github.com>
2020-12-09 11:35:00 +0100

    Add element creation script (#1)
    * add basic function for creating todo elements
    
    * add text and delete HTML creation to createElement
    
    * add event listener for marking as done
    
    * add correct css classes/ids to element creation
    
    * add edit button to element creation

---
+++ script.js
@@ -0,0 +1,44 @@
+const toDoList = document.getElementById("task-list");
+
+/**
+ * creates a new todo list element.
+ *
+ * @param {string} name - name of the element
+ */
+const createElement = (name) => {
+  //create li node
+  const element = document.createElement("LI");
+
+  //create text span node
+  const textSpan = document.createElement("SPAN");
+  textSpan.className = "text";
+  textSpan.appendChild(document.createTextNode(name));
+  //add done event listener
+  textSpan.addEventListener("click", () => {
+    element.classList.toggle("checked");
+  });
+  //add text span to element
+  element.appendChild(textSpan);
+
+  //create edit button node
+  const editSpan = document.createElement("SPAN");
+  editSpan.className = "edit"; //TODO: add classes
+  editSpan.appendChild(document.createTextNode("edit")); //TODO: replace with icon
+  //TODO: generate edit button event listener
+  //add edit button to element
+  element.appendChild(editSpan);
+
+  //create delete button node
+  const deleteSpan = document.createElement("SPAN");
+  deleteSpan.className = "delete";
+  deleteSpan.appendChild(document.createTextNode("delete")); //TODO: replace with icon
+  //TODO: generate delete button event listener
+  //add delete button to element
+  element.appendChild(deleteSpan);
+
+  // add the li node to the todo list
+  toDoList.appendChild(element);
+};
+
+createElement("test1");
+createElement("test2");
