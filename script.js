d110c546509a1fb0260b28a90c69a69e5bb3c06c
g-weiher <74683223+g-weiher@users.noreply.github.com>
2020-12-09 10:41:46 +0100

    add text and delete HTML creation to createElement

--- script.js
+++ script.js
@@ -2,18 +2,32 @@ const toDoList = document.getElementById("tasks");
 
 /**
  * creates a new todo list element.
- * 
+ *
  * @param {string} name - name of the element
  */
 const createElement = (name) => {
+  //create li node
+  const element = document.createElement("LI");
 
-    //create li node
-    const element = document.createElement("LI");
+  //create text span node
+  const textSpan = document.createElement("SPAN");
+  textSpan.className = ""; //TODO: add classes
+  textSpan.appendChild(document.createTextNode(name));
 
-    // add the li node to the todo list
-    toDoList.appendChild(element);
-}
+  //add text span to element
+  element.appendChild(textSpan);
+
+  //create delete button node
+  const deleteSpan = document.createElement("SPAN");
+  deleteSpan.className = ""; //TODO: add classes
+  deleteSpan.appendChild(document.createTextNode("delete")); //TODO: replace with icon
+
+  //add delete button to element
+  element.appendChild(deleteSpan);
+
+  // add the li node to the todo list
+  toDoList.appendChild(element);
+};
 
 createElement("test1");
 createElement("test2");
-
