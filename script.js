3962663393f9810c8cea98e9b32ffec67032a4b5
g-weiher <74683223+g-weiher@users.noreply.github.com>
2020-12-10 13:24:50 +0100

    add basic drag and drop functionality

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const toDoList = document.getElementById("task-list");
 
+//id counter to create unique ids of li elements
+let idCounter = 0;
+
+//needed to prevent unwanted browser behavior when dragging
+window.addEventListener(
+  "dragover",
+  (e) => {
+    e.preventDefault();
+  },
+  false
+);
+window.addEventListener(
+  "drop",
+  (e) => {
+    e.preventDefault();
+  },
+  false
+);
+
 /**
  * creates a new todo list element.
  *
@@ -36,9 +55,36 @@ const createElement = (name) => {
   //add delete button to element
   element.appendChild(deleteSpan);
 
+  //add drag and drop functionality
+  element.id = "li-" + idCounter++;
+  element.draggable = "true";
+  element.addEventListener("dragstart", (ev) => {
+    console.log(element.id);
+    ev.dataTransfer.setData("text", element.id);
+    ev.dataTransfer.dropEffect = "move";
+  });
+  element.addEventListener("dragover", (ev) => {
+    console.log(element.id);
+    ev.preventDefault();
+    element.classList.add("drag");
+  });
+  element.addEventListener("dragleave", () => {
+    element.classList.remove("drag");
+  });
+  element.addEventListener("drop", (ev) => {
+    console.log("droped");
+    toDoList.insertBefore(
+      document.getElementById(ev.dataTransfer.getData("text")),
+      element
+    );
+  });
+
   // add the li node to the todo list
   toDoList.appendChild(element);
 };
 
 createElement("test1");
 createElement("test2");
+createElement("test3");
+createElement("test4");
+createElement("test5");
