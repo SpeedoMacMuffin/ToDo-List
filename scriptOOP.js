29c997dfa488e0f1eb3166f4457959722a61df00
g-weiher <74683223+g-weiher@users.noreply.github.com>
2020-12-17 21:58:22 +0100

    clean up script code (#25)

--- scriptOOP.js
+++ scriptOOP.js
@@ -3,12 +3,7 @@
 let idCounter = 0;
 class ToDoList {
   constructor() {
-    this._list = [
-      new TodoListElement("title1"),
-      new TodoListElement("title2"),
-      new TodoListElement("title3"),
-      new TodoListElement("title4"),
-    ];
+    this._list = [];
   }
 
   // find index of Element with given ID
@@ -68,31 +63,24 @@ class TodoListElement {
 
   render() {
     const htmlElement = document.createElement("LI");
-
+    htmlElement.id = this._id;
 
     //create text span node
     const textSpan = document.createElement("SPAN");
     textSpan.className = "text";
     textSpan.appendChild(document.createTextNode(this._title));
     htmlElement.appendChild(textSpan);
-    htmlElement.id = this._id;
 
-//create delete button
-const deleteSpan = document.createElement("SPAN");  //<span></span>
-deleteSpan.className = "delete";  //<span class ="delete"></span>
-//generate delete button event listener
-deleteSpan.addEventListener("click", () => {
-  this._deleteHandler(this._id);
-});
+    //create delete button
+    const deleteSpan = document.createElement("SPAN");  //<span></span>
+    deleteSpan.className = "delete";  //<span class ="delete"></span>
+    //generate delete button event listener
+    deleteSpan.addEventListener("click", () => {
+      this._deleteHandler(this._id);
+    });
 
-//add delete button to element
-htmlElement.appendChild(deleteSpan);
-
-
-
-
-
-
+    //add delete button to element
+    htmlElement.appendChild(deleteSpan);
 
     //add done event listener to li element
     htmlElement.addEventListener("click", (ev) => {
@@ -104,16 +92,18 @@ htmlElement.appendChild(deleteSpan);
       }
     });
 
-    //TODO rest
     return htmlElement;
   }
 }
 
 const todoList = new ToDoList();
-todoList.renderElements();
-todoList.addElement("title10");
-// todoList.deleteElement("li-1");
-/* Add Button functionality */
+todoList.addElement("Talk about our lord and savior Jesus Christ");
+todoList.addElement("Deactivate addblocker");
+todoList.addElement("Accept all cookies");
+todoList.addElement("Take part in a survey");
+todoList.addElement("Skip morning coffee");
+
+//add button-listener
 const addBttn = document.getElementById("add");
 
 addBttn.addEventListener("click", (e) => {
@@ -126,117 +116,3 @@ addBttn.addEventListener("click", (e) => {
     document.querySelector("#task").value = "";
   }
 });
-
-
-
-//  console.log(todoList.indexOfElement('li-1'));
-
-/*class TodoList {
-  constructor() {
-    this._listElements = [];
-  }
-  createList() {
-    const id = 2;
-
-    const element = new ToDoElement();
-  }
-
-  deleteToDo(id) {
-    for (let i = 0; i < this._listElements.length; i++) {
-      if (this._listElements[i]._id === id) {
-        //delete that thing
-        break;
-      }
-    }
-    document.getElementById;
-
-    console.log(id);
-    //delete element with id from dom and list
-  }
-}
-class ToDoElement {
-  constructor(title = "task", deleteHandler) {
-    this._id = 5;
-    this._title = title;
-    this._done = false;
-    this._htmlElement = null;
-    this._deleteHandler = deleteHandler;
-  }
-  setTitle(title) {
-    this._title = title;
-    this._htmlElement.firstElementChild.innerHTML = title;
-  }
-
-  createListElement() {
-    //create li node
-    const element = document.createElement("LI");
-    element.setAttribute("id", this._id.toString());
-
-    //create text span node
-    const textSpan = document.createElement("SPAN");
-    textSpan.className = "text";
-    textSpan.appendChild(document.createTextNode(this._title));
-    //add done event listener to li element
-    element.addEventListener("click", (ev) => {
-      if (
-        ev.target === element ||
-        (ev.target === textSpan && ev.target.isContentEditable === false)
-      ) {
-        element.classList.toggle("checked");
-      }
-    });
-
-    // addEvent-Listener to supress the return-Key from beeing pressed
-    textSpan.addEventListener("keydown", (keyInfo) => {
-      // stop editing if the Enter-Key is pressed
-      if (keyInfo.keyCode == 13) {
-        // console.log('the Return-Key on the Keyboard has the keydown-Number: 13!');
-        textSpan.contentEditable = "false";
-        editSpan.classList.remove("saveActive");
-      }
-    });
-    //add text span to element
-    element.appendChild(textSpan);
-
-    //create edit button node
-    const editSpan = document.createElement("SPAN");
-    editSpan.className = "edit"; //TODO: add classes
-    // add event listener to the button
-    editSpan.addEventListener("click", () => {
-      // Deactivate the Editable field and hide save Button
-      if (textSpan.contentEditable == "true") {
-        editSpan.classList.remove("saveActive");
-        textSpan.contentEditable = "false";
-      } else {
-        // Show  the Editable field and hide save Button to make the content editable for the textSpan
-        textSpan.contentEditable = "true";
-        editSpan.classList.add("saveActive");
-        textSpan.focus();
-        // PLACE CURSOR AT THE END OF THE CURRENT TEXT
-        // select all the content in the element
-        document.execCommand("selectAll", false, null);
-        // collapse selection to the end
-        document.getSelection().collapseToEnd();
-      }
-    });
-    //add edit button to element
-    element.appendChild(editSpan);
-
-    //create delete button node
-    const deleteSpan = document.createElement("SPAN");
-    deleteSpan.className = "delete";
-    //generate delete button event listener
-    deleteSpan.addEventListener("click", () => this._deleteHandler(this));
-    //add delete button to element
-    element.appendChild(deleteSpan);
-    this._htmlElement = element;
-
-    return element;
-  }
-}
-const element = new ToDoElement("sometitle", () => {
-  console.log("delete");
-});
-
-const toDoList = document.getElementById("task-list");
-toDoList.prepend(element.createListElement());*/
